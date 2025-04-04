a53145cab6b2458c4cc52a92a5ea0b79dfe86752
DONCHAN1234 <arpan70047@gmail.com>
2025-04-05 01:49:25 +0530

    AurdinoSetup

--- W.py
+++ W.py
@@ -270,28 +270,7 @@ def insert_sensor_data2(smoke, temperature, fire):
 
 
 
-########################################################### Connect to Arduino via Serial ################################################
-"""def find_arduino():
-    ports = serial.tools.list_ports.comports()
-    for port in ports:
-        if "Arduino" in port.description or "CH340" in port.description:  # Adjust for your board type
-            return port.device
-    return None
 
-def connectSerial():
-    arduino_port = find_arduino()
-    if arduino_port:
-        try:
-            ser = serial.Serial(arduino_port, 9600, timeout=1)
-            print(f"Connected to Arduino on {arduino_port}")
-            return ser
-        except serial.SerialException as e:
-            print(f"Connection error: {e}")
-            return None
-    else:
-        print("Arduino not found.")
-        return None  """
-########################################################### Connect to Arduino via Serial ################################################
 
 ############################################################ data from flask app backend to fatch ##########################################
 flask_url = "https://aurdinodatatransferingbackend-4.onrender.com/get_data"  # Replace with the Flask endpoint
@@ -447,14 +426,7 @@ def start_camera():
     temperatureC = 0.0
     analogTemp = 0.0
     count = 0
-    """while cap.isOpened():
-        line = ser.readline().decode('utf-8').strip()
-        if line:
-            try:
-                temperatureC, analogTemp = map(float, line.split(','))
-                print(f"🌡 Temperature: {temperatureC}°C, 💨 Smoke: {analogTemp}ppm")
-            except ValueError:
-                print("⚠️ Invalid sensor data received.") """
+    
         
         #smoke_placeholder = st.empty()
         #temp_placeholder = st.empty()
